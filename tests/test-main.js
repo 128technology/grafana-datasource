25f826fb13e2a1d8c8a400730a4e412250bac0be
Dillon Buchanan <dbuchanan@128technology.com>
2017-03-06 15:00:48 -0500

    Remove jsdom

--- tests/test-main.js
+++ tests/test-main.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 
 import prunk from 'prunk';
-import { jsdom } from 'jsdom';
 import chai from 'chai';
 
 // Mock Grafana modules that are not available outside of the core project
@@ -10,14 +9,6 @@ prunk.mock('./css/query-editor.css!', 'no css, dude.');
 prunk.mock('app/plugins/sdk', {
   QueryCtrl: null,
 });
-
-// Setup jsdom
-// Required for loading angularjs
-global.document = jsdom('<html><head><script></script></head><body></body></html>');
-global.window = global.document.parentWindow;
-global.navigator = {};
-window.navigator = {};
-global.Node = window.Node;
 
 // Setup Chai
 chai.should();
